1b007a4dccba7a0f23475f216e4f1b2f975df374
bigbass1997 <bigbass1997.website@gmail.com>
2015-04-16 19:44:18 -0500

    Added config file specifically for permissions

--- src/main/java/com/bigbass1997/coreperms/PermissionsManager.java
+++ src/main/java/com/bigbass1997/coreperms/PermissionsManager.java
@@ -1,5 +1,26 @@
 package com.bigbass1997.coreperms;
 
-public class PermissionsManager {
+import java.io.File;
+import java.util.ArrayList;
 
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+
+public class PermissionsManager {
+	
+	private static Configuration permsConfig;
+	
+	private static ArrayList<Group> groups;
+	
+	public static void initialize(FMLPreInitializationEvent e){
+		permsConfig = new Configuration(new File(e.getModConfigurationDirectory().getAbsolutePath() + "perms.cfg"));
+		
+		groups = new ArrayList<Group>();
+		
+		populateGroups();
+	}
+	
+	private static void populateGroups(){
+		
+	}
 }
